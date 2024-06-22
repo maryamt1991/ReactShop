c225aaca77f6e4e489e38eb9a975dc06235931d8
Maryam <tavalla1991@gmail.com>
2024-06-21 22:44:52 -0700

    create context

---
+++ src/context/ProductContext.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { createContext,useEffect } from 'react'
+import api from '../services/config';
+
+const productContext=createContext()
+function ProductProvider({children}) {
+    const [products,setProducts]=useState([]);
+    useEffect(()=>{
+       
+            const fetchProducts=async()=>{
+                try {
+                    const response= await api.get("/products")
+                    setProducts(response)  
+                } catch (error) {
+                    console.log(error.message)
+                }
+                
+            }
+        fetchProducts();
+    },[])
+  return (
+    <productContext.Provider value={products}>
+        {children}
+    </productContext.Provider>
+  )
+}
+
+export default ProductProvider
