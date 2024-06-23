c2822c169073d1da266c70961eb8df4f06f6763f
Maryam <tavalla1991@gmail.com>
2024-06-23 00:00:13 -0700

    fetch problem solved

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -1,21 +1,19 @@
 import { createContext,useEffect,useState,useContext} from 'react'
 import api from '../services/config';
+import axios from 'axios';
 
   const ProductContext=createContext()
 function ProductProvider({children}) {
     const [products,setProducts]=useState([]);
     useEffect(()=>{
-       
-            const fetchProducts=async()=>{
-                try {
-                    const response= await api.get("/products")
-                    setProducts(response)  
-                } catch (error) {
-                    console.log(error.message)
-                }
-                
-            }
-        fetchProducts();
+   const fetchProducts=async()=>{
+    try {
+        setProducts(api.get("/products"))
+    } catch (error) {
+       console.log(error.message) 
+    }
+   }
+   fetchProducts();
     },[])
   return (
     <ProductContext.Provider value={products}>
--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,24 @@
 import axios from "axios";
 
-const api=axios.create({baseUrl:"https://fakestoreapi.com"})
-api.interceptors.response.use((response)=>response.data,(error)=>Promise.reject(error))
+//const api=axios.create({baseUrl:"https://fakestoreapi.com"})
+//api.interceptors.response.use(
+    //(response)=>{
+       // console.log(response)
+       // return response.data
+   // }
+//,(error)=>{
+   // console.log(error)
+  //  return Promise.reject(error)})
+  const api=axios.create({baseURL:"https://fakestoreapi.com"})
+api.interceptors.response.use(
+    (response)=>
+      {
+      console.log(response)
+      return response.data;
+    },(error)=>{console.log(error)
+      return Promise.reject(error);
+    }
+     
+  )
 
 export default api
