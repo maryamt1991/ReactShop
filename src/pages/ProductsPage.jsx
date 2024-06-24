7f0bf994bc03e3eb1f3385149218e4e0749b714c
Maryam <tavalla1991@gmail.com>
2024-06-23 23:41:51 -0700

    search part

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -2,23 +2,35 @@ import { useProducts } from "../context/ProductContext";
 import styles from "../pages/ProductsPage.module.css"
 import Card from "../components/Card";
 import Loader from "../components/Loader"
+import { ImSearch } from "react-icons/im";
+import { useState } from "react";
 
 function ProductsPage() {
   const products=useProducts();
   console.log(products)
+  const [search,setSearch]=useState("");
+  const searchHandler=()=>{
+    console.log("first")
+  }
   return (
-    <div className={styles.container}>
-      <div className={styles.products}>
+    <>
+    <div>
+      <input type="text" placeholder="...Search" value={search} onChange={(e)=>setSearch(e.target.value.toLocaleLowerCase().trim())}/>
+      <button onClick={searchHandler}><ImSearch/></button>
+    </div>
+       <div className={styles.container}>
+        <div className={styles.products}>
         {!products.length && <Loader/>}
-        <ul>
+        
         {
 products.map((p)=><Card key={p.id} data={p}/>)
         }
-        </ul>
+       
        
       </div>
       <div>sidebar</div>
     </div>
+    </>
   )
 }
 
