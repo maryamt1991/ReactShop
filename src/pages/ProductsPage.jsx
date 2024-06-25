4a029b614e34a89870a575f3fa147332106b1027
Maryam <tavalla1991@gmail.com>
2024-06-24 22:46:54 -0700

    category part

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -4,6 +4,7 @@ import Card from "../components/Card";
 import Loader from "../components/Loader"
 import { ImSearch } from "react-icons/im";
 import { useState } from "react";
+import { FaListUl } from "react-icons/fa6";
 
 function ProductsPage() {
   const products=useProducts();
@@ -11,6 +12,12 @@ function ProductsPage() {
   const [search,setSearch]=useState("");
   const searchHandler=()=>{
     console.log("first")
+  }
+  const categoryHandler=(event)=>{
+    const {tagName}=event.target;
+    const category=event.target.innerText.toLocaleLowerCase();
+    if (tagName!=="LI") return ;
+    console.log(category)
   }
   return (
     <>
@@ -28,7 +35,19 @@ products.map((p)=><Card key={p.id} data={p}/>)
        
        
       </div>
-      <div>sidebar</div>
+      <div>
+        <div>
+          <FaListUl/>
+          <p>Categories</p>
+        </div>
+        <ul onClick={categoryHandler}>
+          <li>All</li>
+          <li>Elecrtonics</li>
+          <li>jewelry</li>
+          <li>Men's Clothing</li>
+          <li>women's Clothing</li>
+        </ul>
+      </div>
     </div>
     </>
   )
