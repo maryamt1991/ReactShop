f05d290a3dbe3958d08bbe621951b35ab66d9be7
Maryam <tavalla1991@gmail.com>
2024-07-13 08:03:10 -0700

    add layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { Routes , Route, Navigate } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import ProductsPage from "./pages/ProductsPage"
 import DetailsPage from "./pages/DetailsPage"
 import CheckoutPage from "./pages/CheckoutPage"
 import PageNotFound from "./pages/PageNotFound"
 import ProductProvider from "./context/ProductContext"
 import CartProvider from "./context/CartContext"
+import Layout from "./layout/Layout"
 
 
 
@@ -12,18 +13,21 @@ function App() {
 
 
   return (
-    <CartProvider>
-    <ProductProvider>
-<Routes>
-  <Route index element={<Navigate to="/products" replace/>}/>
-  <Route path="/products" element={<ProductsPage/>}/>
-  <Route path="/products/:id" element={<DetailsPage/>}/>
-  <Route path="/checkout" element={<CheckoutPage/>}/>
-  <Route path="/*" element={<PageNotFound/>}/>
- </Routes>
-    </ProductProvider>
-    </CartProvider>
- 
+    
+      <CartProvider>
+        <ProductProvider>
+        <Layout>
+          <Routes>
+            <Route index element={<Navigate to="/products" replace />} />
+            <Route path="/products" element={<ProductsPage />} />
+            <Route path="/products/:id" element={<DetailsPage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+            <Route path="/*" element={<PageNotFound />} />
+          </Routes>
+          </Layout>
+        </ProductProvider>
+      </CartProvider>
+    
   )
 }
 
