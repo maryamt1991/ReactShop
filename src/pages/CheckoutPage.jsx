18614dd174405b214e9130871018e161154f8a79
Maryam <tavalla1991@gmail.com>
2024-07-14 05:43:24 -0700

    basket card

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import BasketCard from "../components/BasketCard";
+import {useCart} from "../context/CartContext"
 
 function CheckoutPage() {
+  const [state,dispatch]=useCart();
+  const clickHandler=(type,payload)=>dispatch({type,payload})
+  console.log(state);
   return (
-    <div>CheckoutPage</div>
+    <div>
+      <div>
+        {state.selectedItems.map(product=><BasketCard key={product.id} data={product} clickHandler={clickHandler}/>)}
+       </div>
+    </div>
   )
 }
 
